4a6e6227cc807fe82e169d4c24ce42875a88e627
Saurav Bhattarai <sauravbhattarai96@gmail.com>
2020-08-22 18:08:16 +0545

    1. Autoplay featured added by clicking the next button at a interval of 6 seconds. 2. Images in the responsive media for 768px has been changed in css (i.e margin: auto;).

--- js/javascript.js
+++ js/javascript.js
@@ -104,30 +104,6 @@ dots.forEach((dot, index) => {
 
 
 // Image Autoplay
-
-var count = 0;
-
 setInterval(function(){
-    document.querySelectorAll("label").checked = true;
-    count++;
-
-    if (count > 3) {
-        count = 0;
-    };
-}, 1000);
-
-
-// var slideIndex = 0;
-// carousel();
-
-// function carousel() {
-//   var i;
-//   var x = document.querySelectorAll(".carousel-slide img");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > x.length) {slideIndex = 1}
-//   x[slideIndex-1].style.display = "block";
-//   setTimeout(carousel, 2000); // Change image every 2 seconds
-// }
+    document.querySelector("#nextBtn").click();
+}, 6000);
